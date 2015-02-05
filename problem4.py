1ddd1234c69c3be89a03891de9069153747a6e3d
Rich Chang <rchang@signal.co>
2015-02-05 00:05:21 -0600

    problem 4

--- problem4.py
+++ problem4.py
@@ -1,6 +1,39 @@
-#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+from math import pow
 
-#Find the largest palindrome made from the product of two 3-digit numbers.
+def is_palindrone(num):
+  num_array = []
+  while num != 0:
+    num_array.append(num%10)
+    num = num/10
+  for i in range(0,len(num_array)/2):
+    if num_array[i] != num_array[-1-i]:
+      return False
+  return True
 
-def is_palidrone(num):
-    pass
+#brute force
+def basic_concept(n):
+  current_max = 0
+  for i in range(0,int(pow(10,n))):
+    for j in range(0,int(pow(10,n))):
+      num = i*j
+      if is_palindrone(num):
+        if num > current_max:
+          current_max = num
+  return current_max
+
+#ENHANCE!
+def make_it_faster(n):
+  current_max = 0
+  for i in range(1,int(pow(10,n))):
+    #don't check twice. don't check 1x2 then again 2x1
+    for j in range(i,int(pow(10,n))):
+      num = i*j
+      #only check if already greater than max
+      if num > current_max:
+        if is_palindrone(num):
+          current_max = num
+  return current_max 
+
+print make_it_faster(3)
+#print basic_concept(3)
+  
