6a338336e31b19cef47e275c92b2240b3408d881
Rich Chang <rchang@signal.co>
2015-02-05 01:05:23 -0600

    finish problem 5

--- problem5.py
+++ problem5.py
@@ -1,14 +1,18 @@
 def brute(n):
-  num = 20
+  num = 0
+  check_range = list(reversed(range(1,n+1)))
   while True:
-    for i in range(1,n+1):
-      if num % i != 0:
-        break
-      else:
-        print i
-        print n
-        if i == n:
-          return num
-      num+=1
-    
-print brute(2)
+    if num != 0 and check_divisible(num, check_range):
+      return num
+    else:
+      #num += n
+      num += 2520 #given smallest number divisible by 1-10.
+                  #adding this makes this solution specific to inputing 20 
+ 
+def check_divisible(num, check_range):
+  for i in check_range:
+    if num % i != 0:
+      return False
+  return True
+
+print brute(20)
